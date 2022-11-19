62771bb18ce54bd1510e60680b3d210daa911e5f
Eugene Selifonov <invisible2k@mail.ru>
2022-11-19 20:46:51 +0100

    * Blob: replaced raw pointers with std::unique_ptr to avoid potential mem-leak in create() (in case std::make_shared() throws) and to simplify memory management; memory allocation failure now results in exception instead of returning empty BlobPtr.

--- include/jbkvs/types/blob.h
+++ include/jbkvs/types/blob.h
@@ -12,17 +12,17 @@ namespace jbkvs::types
     class Blob
         : public detail::NonCopyableMixin<Blob>
     {
-        const uint8_t* _data;
+        std::unique_ptr<const uint8_t[]> _data;
         const size_t _size;
 
     public:
         static BlobPtr create(const uint8_t* data, size_t size);
 
-        const uint8_t* data() const noexcept { return _data; }
+        const uint8_t* data() const noexcept { return _data.get(); }
         size_t size() const noexcept { return _size; }
 
     private:
-        Blob(const uint8_t* data, size_t size) noexcept;
+        Blob(std::unique_ptr<const uint8_t[]>&& data, size_t size) noexcept;
         ~Blob() noexcept;
     };
 
--- src/jbkvs/types/blob.cpp
+++ src/jbkvs/types/blob.cpp
@@ -7,35 +7,29 @@ namespace jbkvs::types
 
     BlobPtr Blob::create(const uint8_t* data, size_t size)
     {
-        uint8_t* dataCopy = new (std::nothrow) uint8_t[size];
-        if (!dataCopy)
-        {
-            return BlobPtr();
-        }
-
-        memcpy(dataCopy, data, size * sizeof(uint8_t));
+        std::unique_ptr<uint8_t[]> dataCopy = std::unique_ptr<uint8_t[]>(new uint8_t[size]); // TODO: C++20: replace with std::make_unique_for_overwrite().
+        memcpy(dataCopy.get(), data, size * sizeof(uint8_t));
 
         struct MakeSharedEnabledBlob : public Blob
         {
-            MakeSharedEnabledBlob(const uint8_t* data, size_t size)
-                : Blob(data, size)
+            MakeSharedEnabledBlob(std::unique_ptr<const uint8_t[]>&& data, size_t size)
+                : Blob(std::move(data), size)
             {
             }
         };
 
-        BlobPtr blob = std::make_shared<MakeSharedEnabledBlob>(dataCopy, size);
+        BlobPtr blob = std::make_shared<MakeSharedEnabledBlob>(std::move(dataCopy), size);
         return blob;
     }
 
-    Blob::Blob(const uint8_t* data, size_t size) noexcept
-        : _data(data)
+    Blob::Blob(std::unique_ptr<const uint8_t[]>&& data, size_t size) noexcept
+        : _data(std::move(data))
         , _size(size)
     {
     }
 
     Blob::~Blob() noexcept
     {
-        delete[] _data;
     }
 
 } // namespace jbkvs::types
