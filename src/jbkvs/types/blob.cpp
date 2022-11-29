9136155fe9f889fcee0553e3f5befe5d87505385
Eugene Selifonov <invisible2k@mail.ru>
2022-11-29 14:23:09 +0100

    * Blob: small code style improvement.

--- src/jbkvs/types/blob.cpp
+++ src/jbkvs/types/blob.cpp
@@ -7,7 +7,7 @@ namespace jbkvs::types
 
     BlobPtr Blob::create(const uint8_t* data, size_t size)
     {
-        std::unique_ptr<uint8_t[]> dataCopy = std::unique_ptr<uint8_t[]>(new uint8_t[size]); // TODO: C++20: replace with std::make_unique_for_overwrite().
+        std::unique_ptr<uint8_t[]> dataCopy(new uint8_t[size]); // TODO: C++20: replace with std::make_unique_for_overwrite().
         memcpy(dataCopy.get(), data, size * sizeof(uint8_t));
 
         BlobPtr blob = create(std::move(dataCopy), size);
